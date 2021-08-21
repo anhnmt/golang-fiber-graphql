c4afdde3389254cfeebefa381035643b25d42a6a
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-08-22 04:16:55 +0700

    Update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,37 +12,21 @@ type configuration struct {
 	Jwt      jwt      `yaml:"jwt"`
 }
 
-func Config() *configuration {
+func Config() (cfg *configuration) {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 
 	viper.AutomaticEnv()
 
-	// config ENV
-
-	// Server
-	configEnv("server.port", "SERVER_PORT")
-
-	// Database
-	configEnv("database.db_driver", "DB_DRIVER")
-	configEnv("database.db_url", "DB_URL")
-
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error reading config file, %s", err)
 	}
 
-	cfg := new(configuration)
+	cfg = new(configuration)
 
 	if err := viper.Unmarshal(cfg); err != nil {
 		log.Fatalf("unable to decode into struct, %v", err)
 	}
 
-	return cfg
-}
-
-func configEnv(input ...string) {
-	err := viper.BindEnv(input...)
-	if err != nil {
-		log.Fatalf("Error BindEnv, %s", err)
-	}
+	return
 }
